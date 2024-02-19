93c2411dde7a48a77dcbb03572c6209630c7c161
zhengjie <jerryxuzhengjie@gmail.com>
2024-02-19 12:40:01 +0800

    update 121

--- no121.ts
+++ no121.ts
@@ -4,6 +4,22 @@
 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
  */
 
+// 66 ms 击败 98.69% 使用 TypeScript 的用户
+// 59.58 MB 击败 24.08% 使用 TypeScript 的用户
+
 function maxProfit(prices: number[]): number {
-    return prices.length
+    let buy_in:number = prices[0]
+    let profit:number = 0
+    for (let i:number = 1; i < prices.length; i++) {
+        if (prices[i] < buy_in) {
+            buy_in = prices[i]
+        } else if (prices[i] - buy_in > profit){
+            profit = prices[i] - buy_in
+        }
+    }
+    return profit
 }
+
+// let prices = [7,1,5,3,6,4]
+let prices = [7,6,4,3,1]
+console.log(maxProfit(prices))
